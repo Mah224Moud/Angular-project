c5d68a2a05db86e1a13f6e463c7fbfce07010a3d
Diallo <daissatou.diallo16@mail.com>
2023-11-27 11:27:49 +0100

    ajout des détailles

--- src/app/afficher-details-tache/afficher-details-tache.component.ts
+++ src/app/afficher-details-tache/afficher-details-tache.component.ts
@@ -9,7 +9,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 
-export class AfficherDetailsTacheComponent  {
-  
+export class AfficherDetailsTacheComponent implements OnInit {
+  taskDetails: any;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    // Simuler des données de tâche (remplacez cela par votre logique de récupération de données)
+    this.taskDetails = {
+      name: "Acheter des courses",
+      creationDate: "2023-11-22",
+      dueDate: "2023-12-01",
+      description: "Acheter des fruits, légumes et produits laitiers.",
+      priority: "Haute",
+      status: "En cours"
+    };
+  }
+
+  markAsDone() {
+    this.taskDetails.status = "Terminée";
+  }
 }
 
