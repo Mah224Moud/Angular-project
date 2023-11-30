3cfd4be13e5001e3e8461861a9a67882718c923b
Mamoudou DIALLO <mamoudoudiallo2397@gmail.com>
2023-12-01 00:54:26 +0100

    added create task verification before submit

--- src/app/create-task/create-task.component.ts
+++ src/app/create-task/create-task.component.ts
@@ -15,21 +15,33 @@ export class CreateTaskComponent {
   id = 3;
 
 
+  status: boolean | undefined;
+  message: string = '';
+
   onSubmit() {
-    const newTask = new Task(
-      this.id++,
-      this.newTaskName,
-      this.newTaskDescription,
-      new Date(), 
-      new Date(this.newTaskDeadline), 
-      this.newTaskPriority,
-      0 
-    );
+    if (this.newTaskName && this.newTaskPriority && this.newTaskDeadline) {
+      const newTask = new Task(
+        this.id++,
+        this.newTaskName,
+        this.newTaskDescription,
+        new Date(), 
+        new Date(this.newTaskDeadline), 
+        this.newTaskPriority,
+        0 
+      );
 
-    
-    this.taskCreated.emit(newTask);
+      this.taskCreated.emit(newTask);
+      this.resetForm();
+      this.status = true;
+      this.message = 'La tâche a été créée avec succès.';
+    } else {
+      this.status = false;
+      this.message = 'Veuillez remplir tous les champs.';
+    }
+  }
 
-    this.resetForm();
+  closeAlert() {
+    this.status = undefined;
   }
 
   resetForm() {
