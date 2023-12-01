2d6b8397d9fdfb6d9e8f8058a81c4b8c597fad35
Mamoudou DIALLO <mamoudoudiallo2397@gmail.com>
2023-12-01 14:55:58 +0100

    added archives and it works

--- src/app/archives/archives.component.ts
+++ src/app/archives/archives.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../shared/models/Task';
+import { TaskService } from '../shared/services/task.service';
 
 @Component({
   selector: 'app-archives',
   templateUrl: './archives.component.html',
   styleUrls: ['./archives.component.css']
 })
-export class ArchivesComponent {
+export class ArchivesComponent implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit() {
+    this.tasks = this.taskService.getCompletedTasks();
+  }
 }
