1ca45870b037039f45277ed68d6677a88b1fe5ac
cristanemir <cristanemir@gmail.com>
2023-12-06 14:00:07 +0100

    ajout du tri par rapport a la priorite et le statut de la tache tri sur la date (en cours)

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -21,4 +21,27 @@ export class TaskListComponent {
     this.messageService.changeStatusMessage(task.getName() + ' a été marquée comme terminée.');
     this.tasks = this.tasks.filter(t => t.getId() !== task.getId());
   }
+
+  sortDate(){
+    this.tasks.sort((a, b) => {
+      const dateA = new Date(a.getEnd());
+      const dateB = new Date(b.getEnd());
+      return dateA.getTime() - dateB.getTime();
+    });
+  }
+
+  sortPriority(){
+    this.tasks.sort((a, b) => {
+      const priorities = [ 'élevée', 'moyenne', 'faible'];
+      return priorities.indexOf(a.getPriority()) - priorities.indexOf(b.getPriority());
+    });
+  }
+
+  sortSatut(){ 
+    this.tasks.sort((a, b) => {
+      const status = [ 'en cours', 'terminée'];
+      return status.indexOf(a.getStatus()) - status.indexOf(b.getStatus());
+    });
+  }
+
 }
