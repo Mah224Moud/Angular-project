694cfc03343861a2ca7ae262bf2d9ecece9ef640
Mamoudou DIALLO <mamoudoudiallo2397@gmail.com>
2023-12-23 00:16:21 +0100

    Added directives for car hover

--- src/app/shared/directives/highlight-row.directive.ts
+++ src/app/shared/directives/highlight-row.directive.ts
@@ -8,14 +8,24 @@ export class HighlightRowDirective {
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.renderer.addClass(this.el.nativeElement, 'highlight-row');
+    this.renderer.removeClass(this.el.nativeElement, 'border-danger');
+    this.renderer.addClass(this.el.nativeElement, 'bg-warning');
+    this.renderer.addClass(this.el.nativeElement, 'border-primary');
+    this.changeLinkColor('btn-outline-primary', 'btn-outline-light');
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.renderer.removeClass(this.el.nativeElement, 'highlight-row');
+    this.renderer.removeClass(this.el.nativeElement, 'bg-warning');
+    this.renderer.removeClass(this.el.nativeElement, 'border-primary');
+    this.renderer.addClass(this.el.nativeElement, 'border-danger');
+    this.changeLinkColor('btn-outline-light', 'btn-outline-primary');
   }
 
-  @HostListener('click') onClick() {
-    this.renderer.addClass(this.el.nativeElement, 'highlight-row-clicked');
+  private changeLinkColor(oldColor: string, newColor: string) {
+    const links = this.el.nativeElement.querySelectorAll('a');
+    links.forEach((link: Element) => {
+      this.renderer.removeClass(link, oldColor);
+      this.renderer.addClass(link, newColor);
+    });
   }
 }
